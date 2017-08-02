d080ab649069d09270d5b375d924b4ca3ac12d4e
Ken Siu <kensiu20042000@gmail.com>
2017-08-02 15:30:40 +0800

    create necessary ui components and connect them with code

---
+++ MVVMRx/Login/View/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  MVVMRx
+//
+//  Created by Kam Hei Siu on 2/8/2017.
+//  Copyright © 2017 Kam Hei Siu. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTxtField: UITextField!
+    @IBOutlet weak var passswordTxtField: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
+
