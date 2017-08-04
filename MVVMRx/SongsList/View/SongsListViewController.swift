97efbbe7efb4b659f1e39bb72ab15c1737e0c1f6
Ken Siu <kensiu20042000@gmail.com>
2017-08-04 12:45:15 +0800

    deselect a row when it is selected

--- MVVMRx/SongsList/View/SongsListViewController.swift
+++ MVVMRx/SongsList/View/SongsListViewController.swift
@@ -39,4 +39,8 @@ extension SongsListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return songsListViewModel.rowHeight
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
